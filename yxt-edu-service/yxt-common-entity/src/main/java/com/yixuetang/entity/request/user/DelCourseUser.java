baf31fa3c24e8513d94727081694f70e9b374f77
Colin <colinchong@163.com>
2020-11-04 16:43:24 +0800

    feat: 完成前台门户学生退课/教师删除课程功能

--- yxt-edu-service/yxt-common-entity/src/main/java/com/yixuetang/entity/request/user/DelCourseUser.java
+++ yxt-edu-service/yxt-common-entity/src/main/java/com/yixuetang/entity/request/user/DelCourseUser.java
@@ -1,17 +1,21 @@
 package com.yixuetang.entity.request.user;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * @author Colin
  * @version 1.0.0
- * @description TODO
+ * @description 删除课程的用户实体类
  * @date 2020/11/4 14:59
  */
 @Data
 public class DelCourseUser {
 
+    @ApiModelProperty(value = "用户id", required = true, dataType = "String")
     private Long userId;
+
+    @ApiModelProperty(value = "用户密码", required = true, dataType = "String")
     private String password;
 
 }
